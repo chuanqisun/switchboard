7df97db0a0d379184fb2d6db6367ab7c3b007a9b
Chuánqí Sun <chusun@microsoft.com>
2020-01-13 13:49:14 -0800

    move style above content

--- components/header.js
+++ components/header.js
@@ -18,21 +18,6 @@ function AppHeader() {
 
   return html`
     ${Minimize} ${Close}
-    <header id="header" class="header body--fixed-top">
-      <div class="header__title">Switchboard</div>
-      <div class="header__actions">
-        <button class="button button--shell" @click=${onMinimize}>
-          <svg width="0.6rem" height="0.6rem">
-            <use xlink:href="#svg-minimize" />
-          </svg>
-        </button>
-        <button class="button button--shell" @click=${onClose}>
-          <svg width="0.6rem" height="0.6rem">
-            <use xlink:href="#svg-close" />
-          </svg>
-        </button>
-      </div>
-    </header>
     <style>
       .body--fixed-top {
         flex: 0 0 auto;
@@ -86,6 +71,21 @@ function AppHeader() {
       }
     </style>
     ${FocusVisibleStyle}
+    <header id="header" class="header body--fixed-top">
+      <div class="header__title">Switchboard</div>
+      <div class="header__actions">
+        <button class="button button--shell" @click=${onMinimize}>
+          <svg width="0.6rem" height="0.6rem">
+            <use xlink:href="#svg-minimize" />
+          </svg>
+        </button>
+        <button class="button button--shell" @click=${onClose}>
+          <svg width="0.6rem" height="0.6rem">
+            <use xlink:href="#svg-close" />
+          </svg>
+        </button>
+      </div>
+    </header>
   `;
 }
 
