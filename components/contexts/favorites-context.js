aa38c6d2894d38c9048de210fe9c15153237868a
Chuánqí Sun <chusun@microsoft.com>
2020-01-29 09:07:11 -0800

    clean up ids

--- components/contexts/favorites-context.js
+++ components/contexts/favorites-context.js
@@ -1,6 +1,7 @@
-import { createContext, useState, component, useEffect } from '../../lib/haunted.js';
+import { createContext, useState, component, useEffect, useContext } from '../../lib/haunted.js';
 import { html } from '../../lib/lit-html.js';
 import { ensureUserSettings, addFavorite, removeFavorite, saveUserSettings, isFavorite } from '../../helpers/user-settings.js';
+import { EnvironmentsContext } from '../contexts/environments-context.js';
 
 export const FavoritesContext = createContext({
   status: 'loading',
@@ -13,22 +14,31 @@ customElements.define('sb-favorites-provider-internal', FavoritesContext.Provide
 
 function FavoritesProvider() {
   const [favorites, setFavorites] = useState([]);
+  const [rawFavorites, setRawFavorites] = useState([]);
+  const environmentsContext = useContext(EnvironmentsContext);
   const [status, setStatus] = useState('loading'); // 'loading' | 'loaded'
 
   useEffect(async () => {
     const userSettings = await ensureUserSettings();
-    setFavorites(userSettings.favorites);
-    setStatus('loaded');
+    setRawFavorites(userSettings.favorites);
   }, []);
+
+  useEffect(async () => {
+    if (environmentsContext.status === 'loaded') {
+      const effectiveFavorites = rawFavorites.filter(rawFavorite => environmentsContext.environments.find(environment => environment.appId === rawFavorite));
+      setFavorites(effectiveFavorites);
+      setStatus('loaded');
+    }
+  }, [rawFavorites, environmentsContext.status]);
 
   const addFavoriteInContext = async appId => {
     const userSettings = addFavorite(appId);
-    setFavorites(userSettings.favorites);
+    setRawFavorites(userSettings.favorites);
     await saveUserSettings(userSettings);
   };
   const removeFavoriteInContext = async appId => {
     const userSettings = removeFavorite(appId);
-    setFavorites(userSettings.favorites);
+    setRawFavorites(userSettings.favorites);
     await saveUserSettings(userSettings);
   };
 
