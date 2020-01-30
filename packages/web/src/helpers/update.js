7d0aa71981cfc14c05d1a7f54d45939e9484ae3d
Chuánqí Sun <chusun@microsoft.com>
2020-01-30 15:18:31 -0800

    handle error in get version

--- packages/web/src/helpers/update.js
+++ packages/web/src/helpers/update.js
@@ -2,18 +2,28 @@ const { app } = require('electron').remote;
 import { getMetadata } from './metadata.js';
 
 export async function getVersionSummary() {
-  const metadata = await getMetadata();
-  const { supportedAppVersions } = metadata;
-  const currentVersion = app.getVersion();
-  const isCurrentVersionSupported = supportedAppVersions.includes(currentVersion);
-  const latestVersion = supportedAppVersions.length && supportedAppVersions[supportedAppVersions.length - 1];
-  const isUpdateAvailable = latestVersion !== currentVersion;
-  const isUpdatedRequired = isUpdateAvailable && !isCurrentVersionSupported;
+  try {
+    const metadata = await getMetadata();
+    const { supportedAppVersions } = metadata;
+    const currentVersion = app.getVersion();
+    const isCurrentVersionSupported = supportedAppVersions.includes(currentVersion);
+    const latestVersion = supportedAppVersions.length && supportedAppVersions[supportedAppVersions.length - 1];
+    const isUpdateAvailable = latestVersion !== currentVersion;
+    const isUpdatedRequired = isUpdateAvailable && !isCurrentVersionSupported;
 
-  return {
-    currentVersion,
-    latestVersion,
-    isUpdateAvailable,
-    isUpdatedRequired,
-  };
+    return {
+      currentVersion,
+      latestVersion,
+      isUpdateAvailable,
+      isUpdatedRequired,
+    };
+  } catch(e) {
+    console.log('[update] get version summary failed');
+    return {
+      currentVersion: 'unknown',
+      latestVersion: 'unknown',
+      isUpdateAvailable: false,
+      isUpdatedRequired: false,
+    }
+  }
 }
