55dc03104f6556d5371fe2f14b7f6143d41527e3
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-07-20 10:59:26 -0700

    automatically update envrionment every 60 seconds

--- packages/web/src/contexts/environments-context.js
+++ packages/web/src/contexts/environments-context.js
@@ -1,5 +1,6 @@
 import { html, createContext, useState, component, useEffect } from '../lib/index.js';
 import { getEnvironments, rejectReasonSignedOut, rejectReasonInvalidJson } from '../helpers/environments.js';
+import { useInterval } from '../hooks/use-interval.js';
 
 export const EnvironmentsContext = createContext({
   status: 'loading',
@@ -35,6 +36,27 @@ function EnvironmentsProvider() {
       }
     }
   }, []);
+
+  // try refresh environments every 60 seconds. Avoid updating states or unstable network connection may affect UI
+  useInterval(async () => {
+    try {
+      const environments = await getEnvironments();
+      if (Array.isArray(environments)) {
+        setEnvironments(environments);
+      } else {
+        console.log('[environments-context] parse environments json is not an array');
+      }
+    } catch (e) {
+      switch (e) {
+        case rejectReasonSignedOut:
+          console.log('[environments-context] not signed in');
+          break;
+        case rejectReasonInvalidJson:
+          console.log('[environments-context] parse environments json failed');
+          break;
+      }
+    }
+  }, 60 * 1000);
 
   const contextValue = {
     status,
---
+++ packages/web/src/hooks/use-interval.js
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from '../lib/index.js';
+
+/**
+ * Invoke the callback functino at an interval
+ * @param {() => any} callback
+ * @param {number | null} interval in ms
+ */
+export function useInterval(callback, interval) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      if (!savedCallback.current) return;
+
+      savedCallback.current();
+    }
+    if (interval !== null) {
+      let id = setInterval(tick, interval);
+      return () => clearInterval(id);
+    }
+  }, [interval]);
+}
